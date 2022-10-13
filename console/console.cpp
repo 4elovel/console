14ef00eb90947747dc1ae59ce41e0c1d6c431ed0
4elovel <marcynakvova007@gmail.com>
2022-10-13 18:09:45 +0300

    1234

--- console/console.cpp
+++ console/console.cpp
@@ -5,4 +5,6 @@ int main() {
 	system("md ITEfolder2");
 	system("md ITEfolder3");
 	system("md ITEfolder3\\ITEfolder4\\ITEfolder5");
+	system("echo Я вивчаю програмування > doc1.txt");
+	system("more doc1.txt");
 }
