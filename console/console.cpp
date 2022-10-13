2ef98bde29aeffcc5cb1df29d154cbaf2bcdf04f
4elovel <marcynakvova007@gmail.com>
2022-10-13 18:02:59 +0300

    Добавьте файлы проекта.

---
+++ console/console.cpp
@@ -0,0 +1,8 @@
+﻿#include <iostream>
+
+int main() {
+	system("md ITEfolder1");
+	system("md ITEfolder2");
+	system("md ITEfolder3");
+	system("md C:\\Users\\User\\source\\repos\\console\\console\\ITEfolder3\\ITEfolder4\\ITEfolder5");
+}
